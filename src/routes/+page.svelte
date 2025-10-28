29ae621885a1700c2c58c09a8ad630a060699c0c
Filipy <filipytav17@gmail.com>
2025-10-28 11:27:27 -0300

    Frosted glass effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
 </nav>
 
 <section class="">
-    <div class="m-auto mt-3 bg-gray-200 w-4/5 h-[80vh] rounded-lg">
+    <div class="m-auto mt-3 w-4/5 h-[80vh] rounded-lg bg-white/20 backdrop-blur-sm p-6 shadow-lg">
 
     </div>
 </section>
