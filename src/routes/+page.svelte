ba5a92756f63e5c1dc10cbedb21828c8b03760ac
Filipy <filipytav17@gmail.com>
2025-10-28 09:54:01 -0300

    Initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import logo from "$lib/assets/logo.png";
+</script>
+
+<nav class="bg-black relative min-h-15">
+    <ul class="flex absolute top-0 bottom-0 left-0 right-0 items-center justify-center">
+        <!-- <li class="">
+            <img class="text-white w-15" src={logo} alt="logo.png">
+        </li> -->
+        <li>
+            <p class="text-white text-lg">
+                Identificação de som
+            </p>
+        </li>
+    </ul>
+</nav>
