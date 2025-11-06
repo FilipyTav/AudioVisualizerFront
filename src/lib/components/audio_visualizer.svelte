1db989599b1bbfa5c6c9fa26739e729d3f4f46ea
Filipy <filipytav17@gmail.com>
2025-11-06 11:53:45 -0300

    Improve styling

--- src/lib/components/audio_visualizer.svelte
+++ src/lib/components/audio_visualizer.svelte
@@ -22,11 +22,10 @@
 	class="absolute inset-0 rounded-full bg-purple-400 opacity-75 animate-pulse"
 	style="pointer-events: none;"
 	></div> -->
-
 	<div class="
 	rounded-full w-50 aspect-square inline-block animate-pulse
 	bg-[radial-gradient(circle,rgba(109,11,121,1)_50%,rgba(50,255,100,1)_80%)] 
-	absolute inset-0
+	absolute inset-0 circle-pulse
 	"></div>
 	{#each pings as ping (ping.id)}
 		<div
@@ -49,3 +48,13 @@ before:bg-[radial-gradient(circle,rgba(109,11,121,1)_50%,rgba(50,255,100,1)_80%)
 "> -->
 
 <!-- </div> -->
+
+<style lang="postcss">  
+@reference "tailwindcss";  
+@tailwind utilities;
+
+.circle-pulse {
+@apply bg-[radial-gradient(circle,rgba(109,11,121,1)_50%,rgba(50,255,100,1)_80%)],
+ rounded-full;
+}
+</style>
